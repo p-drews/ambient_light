14e1f7675b20b6328694e51b3790144431012c2c
PatrickDrews <Drews-Patrick@gmx.net>
2016-06-21 09:31:22 +0200

    make it oop

--- AmbientDistControl/src/Daemon/AmbientDistControl.java
+++ AmbientDistControl/src/Daemon/AmbientDistControl.java
@@ -1,195 +1,212 @@
 package Daemon;
 
-import static Daemon.LEDConnector.*;
 import Helper.Conversions;
 import java.io.IOException;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Patze
  */
 public class AmbientDistControl {
-    
-    
-    public static int LEDWidth = 17;
-    
-    public static int green;
-    public static int red;
-    public static int blue = 0;
-    
-    static byte[] color; 
-    
-    private static char side;
-    private static float speed;
-    private static float distance;
-    private static float ttc;
-    public static int pos;
-   
-    
-    public static void main(String[] args) throws Exception {
-        LEDConnector main = new LEDConnector();
-        main.initialize();
-        Thread t = new Thread() {
-            @Override
-            public void run() {
 
-                try {
-                    //Wird gebraucht, damit die Connection aufgebaut werden kann
-                    Thread.sleep(3000);
-                                       
-                    updateData('R');
-                } catch (InterruptedException ex) {
-                    
-                } catch (IOException ex) {
-                    
-                    
-                } 
-            }
-        };
-        t.start();
-        System.out.println("Started");
+  public static int LEDWidth = 17;
+
+  /*
+   public static int green;
+   public static int red;
+   public static int blue = 0;
+
+   static byte[] color;
+   private static char side;
+   private static float speed;
+   private static float distance;
+   private static float ttc;
+   public static int pos;
+   */
+  public static void main(String[] args) throws Exception {
+    Thread t = new Thread() {
+      @Override
+      public void run() {
+
+        try {
+          AmbientDistControl control = new AmbientDistControl();
+          LEDConnector connector = new LEDConnector();
+          connector.initialize();
+          //Wird gebraucht, damit die Connection aufgebaut werden kann
+          Thread.sleep(3000);
+
+          System.out.println("Started");
+          control.simulateSimulatorInputInLoop(connector);
+
+          //control.updateData(connector, 'R');
+        } catch (InterruptedException ex) {
+          Logger.getLogger(AmbientDistControl.class.getName(), ex.getMessage());
+        }
+      }
+    };
+    t.start();
+  }
+
+  //Ansteuerung der LEDs nahe des Aussenspiegels
+  public void updateData(LEDConnector connector, char side, float ttc) throws InterruptedException, IOException {
+    int green = 0;
+    int red = 0;
+    int blue = 0;
+
+    int maxBrightness = 127;
+
+    //int pos = LEDWidth;
+    float relTTC = ttc / 20;
+    if (relTTC < 0) {
+      return;
+    }
+    if (relTTC > 1) {
+      return;
     }
 
-    //Ansteuerung der LEDs nahe des Aussenspiegels
-    public static void updateData(char side) throws InterruptedException, IOException{
-        // Time to Collision muss noch mit rein 
-        if(side == 'l'){
-            pos = 17;
-            for (int j = 0; j < 127 ; j++)
-            {
-                /* Über die LEDWidth kann die zeit gesteuert werden.
-                    127 ist die Stärker der Farbe, somit wird die Brightness 
-                    von Anfang an heruntergeregelt.
-                */
-                    green = (int) (( 1-((float)j/127))*127);
-                    red = 127 - green;
-                    color = new byte[]{
-                        (byte) ((char) green),
-                        (byte) ((char) red),
-                        (byte) ((char) blue)
-                    };
-                    
-                    //senden der position
-                    sendMessage(new byte[]{(byte) pos});
-                    //senden der RGB-Farben
-                    sendMessage(color);                    
-                    //senden der Beifahrerseite
-                    sendMessage(new byte[]{(byte) side});
+// Time to Collision muss noch mit rein 
+    if (side == 'l') {
 
-                    
-                    //sendMessage(new byte[]{(byte) pos});
-                    Thread.sleep(1000);
-                }
-            
-            
-        } else if(side == 'r'){
-            pos = 17;
-            for (int j = 0; j < 127 ; j++)
-            {
-                /* Über die LEDWidth kann die zeit gesteuert werden.
-                    127 ist die Stärker der Farbe, somit wird die Brightness 
-                    von Anfang an heruntergeregelt.
-                */
-                    red = (int) (( 1-((float)j/127))*127);
-                    green = 127 - red;
-                    color = new byte[]{
-                        (byte) ((char) red),
-                        (byte) ((char) green),
-                        (byte) ((char) blue),
-                    };
-                    
-                    //senden der position
-                    sendMessage(new byte[]{(byte) pos});
-                    //senden der RGB-Farben
-                    sendMessage(color);                    
-                    //senden der Beifahrerseite
-                    sendMessage(new byte[]{(byte) side});
+      green = (int) (relTTC * maxBrightness);
+      red = maxBrightness - green;
+    } else if (side == 'r') {
+      /* Über die LEDWidth kann die zeit gesteuert werden.
+       127 ist die Stärker der Farbe, somit wird die Brightness 
+       von Anfang an heruntergeregelt.
+       */
 
-                    
-                    //sendMessage(new byte[]{(byte) pos});
-                    Thread.sleep(1000);
-                }
+      red = (int) ((1 - relTTC) * maxBrightness);
+      green = maxBrightness - red;
+    } else if (side == 'L') {
 
-        } else if (side == 'L')
-        {
-            
-        }         
     }
+
+    byte[] data = new byte[]{
+      (byte) 254, // 
+      (byte) ((char) red), // color
+      (byte) ((char) green),
+      (byte) ((char) blue),
+      (byte) side // side
+    };
+
+    //senden der position
+    connector.sendMessage(data);
+  }
+
+  private void simulateSimulatorInputInLoop(LEDConnector connector) {
+    float distFrontLeft, distFront, distFrontRight;
+    float distRearLeft, distRear, distRearRight;
+    float speedFrontLeft, speedFront, speedFrontRight;
+    float speedRearLeft, speedRear, speedRearRight;
+    boolean indicatorLeft, indicatorRight;
+    boolean onLeftLane, onRightLane;
+    float ownSpeed;
+
+    // scenario 1: left rear car approaching
+    // init
+    ownSpeed = 100 / 3.6f;
+    speedRearLeft = 120 / 3.6f;
+    speedFront = 80 / 3.6f;
+    distRearLeft = 150;
+    
+    // loop
+    while (true) {
+      try {
+        float ttcRearLeft = distRearLeft / (speedRearLeft - ownSpeed);
+
+        try {
+          updateData(connector, 'l', ttcRearLeft);
+        } catch (InterruptedException | IOException ex) {
+          Logger.getLogger(AmbientDistControl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        distRearLeft -= 0.1 * (speedRearLeft - ownSpeed);
+        if (distRearLeft <= 0) {
+          distRearLeft = 150;
+        }
+        Thread.sleep(100); // ~10Hz update rate
+        
+      } catch (InterruptedException ex) {
+        Logger.getLogger(AmbientDistControl.class.getName()).log(Level.SEVERE, null, ex);
+      }
+    }
+
+  }
 }
 
-    /*Macht keinen Sinn.....
+/*Macht keinen Sinn.....
 
-TESTING:
-            pos = 17; 
-            for (int j = 0; j < 127 ; j++)
-            {
-                //Ansteuerung der LEDs nahe des Aussenspiegels rechts
-                //for (int i = 0; i <= LEDWidth; i++){
-                    red = (int) (( 1-((float)j/127))*127);
-                    green = 127 - red;
-                    color = new byte[]{
-                        (byte) ((char) green),
-                        (byte) ((char) red),
-                        (byte) ((char) blue)
-                    };
+ TESTING:
+ pos = 17; 
+ for (int j = 0; j < 127 ; j++)
+ {
+ //Ansteuerung der LEDs nahe des Aussenspiegels rechts
+ //for (int i = 0; i <= LEDWidth; i++){
+ red = (int) (( 1-((float)j/127))*127);
+ green = 127 - red;
+ color = new byte[]{
+ (byte) ((char) green),
+ (byte) ((char) red),
+ (byte) ((char) blue)
+ };
                     
                     
-                    sendMessage(new byte[]{(byte) pos});
-                    //senden der RGB-Farben
-                    sendMessage(color);
-                    //senden der Beifahrerseite
-                    sendMessage(new byte[]{(byte) side});
-                //}
-            } 
-----------------------------------------------------------------------
+ sendMessage(new byte[]{(byte) pos});
+ //senden der RGB-Farben
+ sendMessage(color);
+ //senden der Beifahrerseite
+ sendMessage(new byte[]{(byte) side});
+ //}
+ } 
+ ----------------------------------------------------------------------
 
 
 
-public static void Fahrzeug(char side, float speed, float distance, char Pattern) throws IOException{
-        TestController.side = side;
-        TestController.speed = speed;
-        TestController.distance = distance;
-        TestController.ttc = distance / speed;
+ public static void Fahrzeug(char side, float speed, float distance, char Pattern) throws IOException{
+ TestController.side = side;
+ TestController.speed = speed;
+ TestController.distance = distance;
+ TestController.ttc = distance / speed;
 
         
-            if(side == 'l'){
-                for(float i = ttc; i > 0; i--){
-                    if (ttc <=20 && ttc >12){
+ if(side == 'l'){
+ for(float i = ttc; i > 0; i--){
+ if (ttc <=20 && ttc >12){
                 
-                    sendMessage(new byte[]{(byte) side});
+ sendMessage(new byte[]{(byte) side});
 
-                    red = 0; green = 127; blue = 0;
+ red = 0; green = 127; blue = 0;
 
-                    color = new byte[]{
-                            (byte) ((char) green),
-                            (byte) ((char) red),
-                            (byte) ((char) blue)
-                    };
-                    sendMessage(color);
+ color = new byte[]{
+ (byte) ((char) green),
+ (byte) ((char) red),
+ (byte) ((char) blue)
+ };
+ sendMessage(color);
 
-                    }
-                    if(ttc <=12 && ttc > 9.5){
-                       sendMessage(new byte[]{(byte) side});
+ }
+ if(ttc <=12 && ttc > 9.5){
+ sendMessage(new byte[]{(byte) side});
 
-                    red = 0; 
-                    green = 127; 
-                    blue = 0;
+ red = 0; 
+ green = 127; 
+ blue = 0;
 
-                    color = new byte[]{
-                            (byte) ((char) green),
-                            (byte) ((char) red),
-                            (byte) ((char) blue)
-                    };
-                    sendMessage(color); 
-                    }
-                }
-            } else if(side == 'r'){
+ color = new byte[]{
+ (byte) ((char) green),
+ (byte) ((char) red),
+ (byte) ((char) blue)
+ };
+ sendMessage(color); 
+ }
+ }
+ } else if(side == 'r'){
 
-            }
+ }
         
-    }*/
-   
+ }*/
 /* Für später:
-    setPattern, Position und Klasse für die Fahrzeuge erstellen
-*/
+ setPattern, Position und Klasse für die Fahrzeuge erstellen
+ */
---
+++ arduino/ba_pattern_mirror/ba-pattern-mirror/ba-pattern-mirror.ino
@@ -0,0 +1,283 @@
+#include <Adafruit_NeoPixel.h>
+#include <Math.h>
+
+#define PIN_left 6 //left
+#define PIN_right 7 //right
+#define BAUD_RATE 9600  // updates per second e.g. 115200 for sparkle,
+// 7200 seems to be stable for ambicar (NeoPixel)
+
+#define PIXEL_COUNT_left 53
+#define PIXEL_COUNT_right 53
+
+//initialize Neopixel
+Adafruit_NeoPixel strip_left = Adafruit_NeoPixel(PIXEL_COUNT_left, PIN_left, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip_right = Adafruit_NeoPixel(PIXEL_COUNT_right, PIN_right, NEO_GRB + NEO_KHZ800);
+
+//set pointer on left strip
+Adafruit_NeoPixel *strip = &strip_right;
+
+//var declaration
+
+int pos = 0;
+
+
+//position declaration
+
+byte val;
+byte green;
+byte red;
+byte blue = 0;
+
+enum states{
+ GET_POSITION,
+ GET_COLOR1,
+ GET_COLOR2,
+ GET_COLOR3,
+ GET_SIDE,
+ MIRROR_LEFT,
+ MIRROR_RIGHT,
+ GROW_LEFT,
+ GROW_RIGHT
+};
+
+
+states current_state = GET_POSITION;
+
+
+
+
+//-------------------------------------Setup/Loop-----------------------
+
+void setup() {
+  strip_left.begin();
+  strip_left.show(); //initialize left pixel
+
+  strip_right.begin();
+  strip_right.show(); //init right pixel
+
+  Serial.begin(9600);// begin serial connection
+
+
+ 
+}
+
+void loop () {
+ strip->show(); 
+}
+
+
+
+//--------------------------------------------------------------------
+
+void growing_left(){
+  strip = &strip_left;
+  for (int i = 0; i < strip->numPixels(); i++){
+    for (int j = 0 ; j <= i ; j++){
+      
+      green = (int)((1-((float)(i)/53)) * 85);    
+      red = 85-green;
+      strip->setPixelColor(j,red, green,0 );
+    }
+
+    strip_left.show();
+    delay(50);
+  }
+
+}
+
+//------------------------------------------------------------------
+
+
+void growing_right(){
+  
+    strip = &strip_right;
+    for (int i = 0; i < strip->numPixels(); i++){
+      for (int j = 0 ; j <= i ; j++){
+        
+        red = (int)((1-((float)(i)/53)) * 85);    
+        green = 85-red;
+        strip->setPixelColor(j,red, green,0 );
+      }
+  
+      strip->show();
+      delay(50);
+    }
+}
+
+//-----------------------------------------------------------------
+void setOff() {
+  for (uint16_t i=0; i< strip->numPixels(); i++){
+    strip->setPixelColor(i, 0,0,0);
+  }
+  strip->show();
+  delay(2000);
+  
+}
+
+//--------------------------Serial-Event-------------------------------
+
+void serialEvent(){ // To check if there is any data on the Serial line
+  while(Serial.available()){
+    byte val = Serial.read();
+    
+    switch(current_state){
+      case GET_POSITION:
+      {
+        switch(val){
+          case 254:
+          {
+            pos = 17;
+            current_state = GET_COLOR1;  
+            break;
+          }
+          default:
+          break;
+        }
+        break;
+      }
+      case GET_COLOR1:
+      {
+        red = val;
+        current_state = GET_COLOR2;
+        break;
+      }
+      case GET_COLOR2:
+      {
+        green = val;
+        current_state = GET_COLOR3;
+        break;
+      }
+      case GET_COLOR3:
+      {
+        blue = val;
+        current_state = GET_SIDE;
+        break;
+      }
+      case GET_SIDE:
+      {
+          switch(val){
+          case 108:
+           {
+            current_state = MIRROR_LEFT;
+            
+            break;
+           }
+           case 114:
+           {
+            current_state = MIRROR_RIGHT;
+            break;
+           }
+           case 76:
+           {
+            current_state = GROW_LEFT;
+            break; 
+           }
+           case 82:
+           {
+            current_state = GROW_RIGHT;
+            break;
+           }
+        }
+        //Dieses Break verursacht 2 Farbfehler (Bei 110,17,0 bis 107,20,0) und bei
+        // (17,110,0)....
+        break;
+      }
+      case MIRROR_LEFT:
+      {
+        strip = &strip_left;
+        
+        for(int i = 0; i <= pos; i++)
+        {
+          
+          strip->setPixelColor(i,red,green,blue);
+        }
+        current_state = GET_POSITION;
+        break;
+      }
+      case MIRROR_RIGHT:
+      {
+        strip = &strip_right;
+        
+        for(int i = 0; i <= pos; i++)
+        {
+          
+          strip->setPixelColor(i,red,green,blue);
+        }
+        current_state=GET_POSITION;
+        break;
+      }
+      case GROW_LEFT:
+      {
+        break;
+      }
+      case GROW_RIGHT:
+      {
+        break;
+      }
+    }
+  }
+}
+/*         
+        strip = &strip_right;
+
+        for(int i = 0; i <= LEDWidth; i++)
+        {
+          
+          strip->setPixelColor(i,red,green,blue);
+        }
+        current_state=WAITING;
+        break;
+
+      }
+      
+    
+      
+
+//------------------------------PATTERN_LEFT---------------------------
+
+      case PATTERN_LEFT:
+      {
+        red = val;
+        current_state = SECOND_COLOR_LEFT;
+        break;
+      }
+      case SECOND_COLOR_LEFT:
+      {
+        green = val;
+        current_state = MIRROR_PAT_LEFT;
+        break;
+      }
+      case MIRROR_PAT_LEFT:
+      {
+        blue = val;
+      }
+      case MIRROR_PAT_LEFT_1:
+      { 
+        strip = &strip_left;
+        pos = val;
+        for(int i = 0; i <= pos; i++)
+        {
+          
+          strip->setPixelColor(i,red,green,blue);
+        }
+        current_state=WAITING;
+        break;
+      }
+      break;
+    }
+  }
+}
+
+
+/* spiegelPat(){
+ *   for (int j = 0 ; j < 127 ; j++){
+    for (int i = 0; i <= ledWidth; i++) {
+        green = (int) ((1-((float)j/127))*127);
+        red = 127 - green;
+        strip->setPixelColor(i, red, green, 0 );
+    }      
+      strip->show();
+      delay(20);
+  };
+ */
+
